0cf52f5d7b4b153ea07a409afbc4e1715a0bd7a0
Margarita <margarita.petrova.kn@gmail.com>
2021-11-30 14:10:24 +0200

    Add router
    Added router for header buttons.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import {} from 'react-router-dom'; 
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Dashboard from './components/Dashboard';
+import Details from './components/Details';
+import Create from './components/Create';
+import Edit from './components/Edit';
+import Login from './components/Login';
+import Register from './components/Register';
+import MyPets from './components/MyPets';
 
 function App() {
   return (
@@ -7,9 +15,18 @@ function App() {
       <Header />
 
       <main id="site-content">
-
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/my-pets" element={<MyPets />} />
+          {/* <Route path="/details" element={<Details />} /> */}
+          <Route path="/create" element={<Create />} />
+          {/* <Route path="/edit" element={<Edit />} /> */}
+        </Routes>
       </main>
 
+      <Footer />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,16 @@ const Header = () => {
         <header id="site-header">
             <nav className="navbar">
                 <section className="navbar-dashboard">
-                    <a href="#">Dashboard</a>
+                    <a href="/">Dashboard</a>
                     <div id="guest">
-                        <a className="button" href="#">Login</a>
-                        <a className="button" href="#">Register</a>
+                        <a className="button" href="/login">Login</a>
+                        <a className="button" href="register">Register</a>
                     </div>
                     <div id="user">
                         <span>Welcome, </span>
-                        <a className="button" href="#">My Pets</a>
-                        <a className="button" href="#">Add Pet</a>
-                        <a className="button" href="#">Logout</a>
+                        <a className="button" href="/my-pets">My Pets</a>
+                        <a className="button" href="/create">Add Pet</a>
+                        <a className="button" href="/logout">Logout</a>
                     </div>
                 </section>
             </nav>
