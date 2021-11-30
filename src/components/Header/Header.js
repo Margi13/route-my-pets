ed5d4b54cc77d54f7cb2a1671fc0e4eb01273c2a
Margarita <margarita.petrova.kn@gmail.com>
2021-11-30 14:16:01 +0200

    Updated the links in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,20 @@
+import { Link, NavLink } from 'react-router-dom'
+
 const Header = () => {
     return (
         <header id="site-header">
             <nav className="navbar">
                 <section className="navbar-dashboard">
-                    <a href="/">Dashboard</a>
+                    <Link to="/">Dashboard</Link>
                     <div id="guest">
-                        <a className="button" href="/login">Login</a>
-                        <a className="button" href="register">Register</a>
+                        <Link to="/login" className="button">Login</Link>
+                        <Link to="/register" className="button">Register</Link>
                     </div>
                     <div id="user">
                         <span>Welcome, </span>
-                        <a className="button" href="/my-pets">My Pets</a>
-                        <a className="button" href="/create">Add Pet</a>
-                        <a className="button" href="/logout">Logout</a>
+                        <Link to="/my-pets" className="button">My Pets</Link>
+                        <Link to="/create" className="button">Add Pet</Link>
+                        <Link to="/logout" className="button">Logout</Link>
                     </div>
                 </section>
             </nav>
